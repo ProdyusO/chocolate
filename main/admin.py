065d244063e07a9cd9568061e49c69f1fe67a58b
Oleksii Prodyus <70231122+ProdyusO@users.noreply.github.com>
2021-12-09 22:31:47 +0200

    Initial commit

---
+++ main/admin.py
@@ -0,0 +1,72 @@
+from PIL import Image
+
+from django.contrib import admin
+from django.forms import ModelChoiceField, ModelForm, ValidationError
+from django.utils.safestring import mark_safe
+
+from .models import *
+
+# class for Validations with resolutions and size
+
+
+class TShirtAdminForm(ModelForm):
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['image'].help_text = mark_safe(
+            '<span style="color:red; font-size:13px;">Завантажуйте зображення з розширенням не менш ніж 500 x 450, якщо більше то само відріже</span>'
+            )
+
+
+class BagAdminForm(ModelForm):
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['image'].help_text = mark_safe(
+            '<span style="color:red; font-size:13px;">Завантажуйте зображення з розширенням не менш ніж 400 x 450, якщо більше то само відріже</span>'
+            )
+
+
+    # def clean_image(self):
+    #     image = self.cleaned_data['image']
+    #     img = Image.open(image)
+    #     min_height, min_width = Product.MIN_RESOLUTION
+    #     max_height, max_width = Product.MAX_RESOLUTION
+    #     if image.size > Product.MAX_IMAGE_SIZE:
+    #         raise ValidationError('Розмір зображення не повинен перевищувати 3 мб')
+    #     if img.height < min_height or img.width < min_width:
+    #         raise ValidationError('Розширення зображення меньше мінімального')
+    #     if img.height > max_height or img.width > max_width:
+    #         raise ValidationError('Розшірення зображення більше максимального')
+    #     return image
+
+
+class TShirtAdmin(admin.ModelAdmin):
+
+    form = TShirtAdminForm
+
+    def formfield_for_foreignkey(self, db_field, request, **kwargs):
+        if db_field.name == 'category':
+            return ModelChoiceField(Category.objects.filter(slug='tshirts'))
+        return super().formfield_for_foreignkey(db_field, request, **kwargs)
+
+
+class BagAdmin(admin.ModelAdmin):
+
+    form = BagAdminForm
+
+    def formfield_for_foreignkey(self, db_field, request, **kwargs):
+        if db_field.name == 'category':
+            return ModelChoiceField(Category.objects.filter(slug='bags'))
+        return super().formfield_for_foreignkey(db_field, request, **kwargs)
+
+
+
+admin.site.register(Category)
+admin.site.register(TShirt, TShirtAdmin)
+admin.site.register(Bag, BagAdmin)
+admin.site.register(CartProduct)
+admin.site.register(Cart)
+admin.site.register(Customer)
+admin.site.register(Order)
+
