1aed7ab3c0af846b6a0d750ae095be9725899976
Oleksii Prodyus <70231122+ProdyusO@users.noreply.github.com>
2021-12-25 00:57:21 +0200

    4th commit
    Back End + Front End

--- chocolate/settings.py
+++ chocolate/settings.py
@@ -91,10 +91,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static_dev'),
-
-)
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static_dev'), )
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
